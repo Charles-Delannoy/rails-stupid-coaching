00cf4d2d59c508886e5765caccfdf73d65d1cfe7
Charles Delannoy <charles.delannoy@hei.fr>
2020-05-11 11:37:09 +0200

    add style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,12 @@
 class QuestionsController < ApplicationController
-
   def ask
   end
 
   def answer
+    if params['question'] == 'I am going to work'
+      @response = 'Great!'
+    else
+      @response = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
